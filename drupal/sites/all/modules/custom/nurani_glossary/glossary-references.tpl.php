2335da9a2255758dfc4e1e901ca99fe3a34a64ee
unknown <Zainab@.(none)>
2012-03-15 10:58:01 +0000

    glossary removed

--- drupal/sites/all/modules/custom/nurani_glossary/glossary-references.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php if ($results) : ?>
-<ul class="glossary-references">
-<?php foreach ($results as $term) : ?>
-<li class="glossary-reference">
-<?php print l($term->title, 'node/' . $term->nid) ?>
-</li>
-<?php endforeach ?>
-</ul>
-<?php endif ?>
