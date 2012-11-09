df3a07d939fd5231ef23c7d9c2b272a44fb02c0a
James Andres <james.andres@gmail.com>
2012-11-09 13:19:40 -0500

    Adding mechanism for rendering comment number.

--- drupal/sites/all/themes/nurani/templates/comment.tpl.php
+++ drupal/sites/all/themes/nurani/templates/comment.tpl.php
@@ -14,6 +14,8 @@
     
     
     <div class="content">
+      <strong>Id: <?php echo $comment_number; ?></strong>
+
       <?php 
         hide($content['links']); 
         print render($content);
