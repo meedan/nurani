ee481d19449c7fba53d2515312b9baee2fbbc379
zainab balogun <zeebalogun@yahoo.com>
2013-01-17 13:26:35 +0000

    theming discussion-add-form and loading  nurani theme  unto the module to share CSS files

--- drupal/sites/all/modules/custom/nurani_helpers/css/config.rb
+++ drupal/sites/all/modules/custom/nurani_helpers/css/config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
+load "/Applications/mamp/htdocs/meedan-nurani/drupal/sites/all/themes/nurani/scss"
 http_path = "/"
 css_dir = "."
 sass_dir = "scss"
