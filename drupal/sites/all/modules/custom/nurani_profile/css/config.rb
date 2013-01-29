ec6b71183acb13a6a7e86b8c1457a4f992db37a9
zainab balogun <zeebalogun@yahoo.com>
2013-01-29 16:16:28 +0000

    theming profile tooltip

---
+++ drupal/sites/all/modules/custom/nurani_profile/css/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+load "/Applications/mamp/htdocs/meedan-nurani/drupal/sites/all/themes/nurani/scss"
+http_path = "/"
+css_dir = "."
+sass_dir = "scss"
+images_dir = "../img"
+javascripts_dir = "../js"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
