b159f765bec1d36dba1903cc588d040fcaaaec28
zainab balogun <zeebalogun@yahoo.com>
2013-02-16 09:22:28 +0000

    Fixing Discussion form text field that breaks

--- drupal/sites/all/modules/custom/nurani_helpers/css/config.rb
+++ drupal/sites/all/modules/custom/nurani_helpers/css/config.rb
@@ -1,6 +1,8 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
-load "/Applications/mamp/htdocs/meedan-nurani/drupal/sites/all/themes/nurani/scss"
+require "sassy-buttons"
+
+load "../../../../themes/nurani"
 http_path = "/"
 css_dir = "."
 sass_dir = "scss"
